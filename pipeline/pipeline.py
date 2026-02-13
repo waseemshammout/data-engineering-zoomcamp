db2c14bed86389fdd2f16111ee7e4abd3e1587e7
waseemshammout <91029813+waseemshammout@users.noreply.github.com>
2026-02-13 10:23:18 +0000

    UV confirmed

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,6 +1,16 @@
 import sys
-import pandas
-print('arguments', sys.argv)
+import pandas as pd
 
+month = int(sys.argv[1])
 
-# print("Rahaf")
+df = pd.DataFrame(
+    {
+        'day': [1,2],
+        'num_pass': [5,6],
+    }
+)
+
+df['month'] = month
+
+df.to_parquet(f'output_{month}.parquet')
+print('Parquet File created!')
